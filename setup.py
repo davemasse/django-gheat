5c88fca295c2f9cfc067a4f32e890477d82aa896
Dave Masse <dmasse@rudemoose.com>
2011-08-21 11:42:21 -0400

    Update setup file to include png files.

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from distutils.core import setup
+
 setup(name='gheat',
       version='1.0',
       url='https://github.com/davemasse/django-gheat',
-      py_modules=[],
-      package_dir = {'': 'gheat'},
+      packages=['gheat'],
+      package_data = {
+        'gheat': ['etc/color-schemes/*','etc/dots/*']
+      },
+      include_package_data=True,
       )
