4292fd3453a6a87f76f33da8a445dcf232077c6c
Dave Masse <dmasse@rudemoose.com>
2011-08-20 22:58:10 -0400

    Add basic setup.py file.

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='gheat',
+      version='1.0',
+      url='https://github.com/davemasse/django-gheat',
+      py_modules=[],
+      package_dir = {'': 'gheat'},
+      )
